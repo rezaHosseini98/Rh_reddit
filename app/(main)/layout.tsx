46dc7c357b15427466243ea5225588ac55cd2ee7
rezaHosseini <rezahosseiny1998@gmail.com>
2026-06-06 13:16:36 +0330

    Implement header & auth & db neon

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,20 @@
+import { LeftSidebar } from "@/components/layout/left-sidebar";
+import { Navbar } from "@/components/layout/navbar";
+import { getSessionUser } from "@/lib/auth";
+
+export default async function MainGroupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await getSessionUser();
+  return (
+    <>
+      <Navbar />
+      <div className="mx-auto flex max-w-300 gap-8 px-4 pb-16 pt-2">
+        <LeftSidebar />
+        <div className="min-w-0 flex-1">{children}</div>
+      </div>
+    </>
+  );
+}
